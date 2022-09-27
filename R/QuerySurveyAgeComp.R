0a00eb87b83ccf6b35783f5b71cfdfcef63f362c
careymcg <carey.mcgilliard@gmail.com>
2022-09-27 16:00:27 -0700

    Updated Survey Age Comp Query
    made it work. still needs formatting.

--- R/QuerySurveyAgeComp.R
+++ R/QuerySurveyAgeComp.R
@@ -1,19 +1,32 @@
 #Query the survey age comp data for unidentified and NRS in the EBS trawl survey
 
 
-SELECT
-haehnr.agecomp_ebs_standard_stratum.species_code,
-haehnr.agecomp_ebs_standard_stratum.year,
-haehnr.agecomp_ebs_standard_stratum.stratum,
-haehnr.agecomp_ebs_standard_stratum.sex,
-haehnr.agecomp_ebs_standard_stratum.age,
-haehnr.agecomp_ebs_standard_stratum.agepop,
-haehnr.agecomp_ebs_standard_stratum.meanlen,
-haehnr.agecomp_ebs_standard_stratum.sdev
-FROM
-haehnr.agecomp_ebs_standard_stratum
-WHERE
-( haehnr.agecomp_ebs_standard_stratum.species_code = 10261
-  AND haehnr.agecomp_ebs_standard_stratum.stratum = 999999 )
-OR ( haehnr.agecomp_ebs_standard_stratum.species_code = 10260
-     AND haehnr.agecomp_ebs_standard_stratum.stratum = 999999 )
+
+SurveyAgeComp<-function(user,pwd) {
+
+AFSC <- odbcConnect("AFSC","mcgilliardc","oopscircle12$") #mcgilliardc
+
+thequery<-paste0("SELECT haehnr.agecomp_ebs_standard_stratum.species_code,\n ",
+"haehnr.agecomp_ebs_standard_stratum.year,\n ",
+"haehnr.agecomp_ebs_standard_stratum.stratum,\n ",
+"haehnr.agecomp_ebs_standard_stratum.sex,\n ",
+"haehnr.agecomp_ebs_standard_stratum.age,\n ",
+"haehnr.agecomp_ebs_standard_stratum.agepop,\n ",
+"haehnr.agecomp_ebs_standard_stratum.meanlen,\n ",
+"haehnr.agecomp_ebs_standard_stratum.sdev\n ",
+"FROM haehnr.agecomp_ebs_standard_stratum\n ",
+"WHERE ( haehnr.agecomp_ebs_standard_stratum.species_code = 10261\n ",
+"  AND haehnr.agecomp_ebs_standard_stratum.stratum = 999999 )\n ",
+"OR ( haehnr.agecomp_ebs_standard_stratum.species_code = 10260\n ",
+"     AND haehnr.agecomp_ebs_standard_stratum.stratum = 999999 )")
+
+ages<-sqlQuery(AFSC,thequery,believeNRows = FALSE)
+
+ages<-ages%>% filter(AGE!=-9)
+agecomp<-ages %>% group_by(YEAR,SEX,AGE) %>% summarise(CAges = sum( AGEPOP))
+agetot<-ages %>% group_by(YEAR,SEX) %>% summarise(TAges = sum( AGEPOP))
+Pagecomp<-full_join(agecomp,agetot) %>% mutate(Prop = CAges/TAges)
+
+#Look up whether it's females then males or vice versa for input to dat file
+return(Pagecomp)
+}
