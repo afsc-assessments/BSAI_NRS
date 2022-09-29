a8e5f41f9a5cddf34db5c9b8e12f2ec9a7125927
careymcg <carey.mcgilliard@gmail.com>
2022-09-29 15:54:59 -0700

    Update to QuerySurveyAgeComp
    Corrected to sum to 1 across F and M together and put males to right of females in formatting

--- R/QuerySurveyAgeComp.R
+++ R/QuerySurveyAgeComp.R
@@ -2,12 +2,11 @@
 library(RODBC)
 library(dplyr)
 library(tidyr)
+AFSC <- odbcConnect("AFSC","mcgilliardc","oopscircle12$") #mcgilliardc
 outdir = "C:\\Users\\carey.mcgilliard\\Work\\FlatfishAssessments\\2022\\NRS\\Data\\Survey_Comps"
 #Example:
-myages<-SurveyAgeComp(user = "mcgilliardc",pwd = "oopscircle12$",outdir = outdir)
-SurveyAgeComp<-function(user,pwd,outdir) {
-
-AFSC <- odbcConnect("AFSC","mcgilliardc","oopscircle12$") #mcgilliardc
+myages<-SurveyAgeComp(DB = AFSC,user = "mcgilliardc",pwd = "oopscircle12$",outdir = outdir)
+SurveyAgeComp<-function(DB,user,pwd,outdir) {
 
 thequery<-paste0("SELECT haehnr.agecomp_ebs_standard_stratum.species_code,\n ",
 "haehnr.agecomp_ebs_standard_stratum.year,\n ",
@@ -23,9 +22,9 @@ thequery<-paste0("SELECT haehnr.agecomp_ebs_standard_stratum.species_code,\n ",
 "OR ( haehnr.agecomp_ebs_standard_stratum.species_code = 10260\n ",
 "     AND haehnr.agecomp_ebs_standard_stratum.stratum = 999999 )")
 
-ages<-sqlQuery(AFSC,thequery,believeNRows = FALSE)
+ages<-sqlQuery(DB,thequery,believeNRows = FALSE)
 
-ages<-ages%>% filter(AGE!=-9,AGE!=-99)
+ages<-ages%>% filter(AGE!=-9,AGE!=-99,SEX!=3,SEX!=9)
 #Bin plus group:
 ages <- ages %>% mutate(AGE = replace(AGE, AGE > 20, 20))
 
@@ -33,21 +32,26 @@ ages <- ages %>% mutate(AGE = replace(AGE, AGE > 20, 20))
 agecomp<-ages %>% group_by(YEAR,SEX,AGE) %>% summarise(CAges = sum( AGEPOP))
 
 #sum to get a total over ages to use for figuring out proportions at age
-agetot<-ages %>% group_by(YEAR,SEX) %>% summarise(TAges = sum( AGEPOP))
+agetot<-ages %>% group_by(YEAR) %>% summarise(TAges = sum( AGEPOP))
 
 #Turn age numbers into proportions
 Pagecomp<-full_join(agecomp,agetot) %>% mutate(Prop = CAges/TAges)
 Pagecomp<-Pagecomp %>% select(YEAR,SEX,AGE,Prop)
-#Look up whether it's females then males or vice versa for input to dat file
 
 
 #organize by females on left and males on right, bin at the plus group age
-thegrid<-expand.grid(AGE=seq(from =1,to=20,by=1),SEX = c(1,2),YEAR = unique(Pagecomp$YEAR))
+#add 100 to male ages to denote they are males
+Pagecomp<-Pagecomp %>% mutate(AGE = replace(AGE, SEX==2,100+AGE))
+Pagecomp<-Pagecomp %>% group_by(YEAR,AGE) %>% select(YEAR,AGE,Prop)
+
+#thegrid<-expand.grid(AGE=seq(from =1,to=20,by=1),SEX = c(1,2),YEAR = unique(Pagecomp$YEAR))
+thegrid<-expand.grid(AGE=c(seq(from =1,to=20,by=1),seq(from = 101,to=120,by=1)),YEAR = unique(Pagecomp$YEAR))
 ExpandComp<-full_join(Pagecomp,thegrid) 
 ExpandComp<-ExpandComp %>% replace_na(list(Prop=0))
 ExpandComp$Prop<-format(round(ExpandComp$Prop,7),nsmall=7)
 
-WideComp<-ExpandComp %>% group_by(SEX,YEAR,AGE) %>% select(YEAR,AGE,Prop) %>% spread(AGE,Prop)
-write.csv(WideComp,file.path(outdir,"SurveyAgeComp.csv"))
+#WideComp<-ExpandComp %>% group_by(SEX,YEAR,AGE) %>% select(YEAR,AGE,Prop) %>% spread(AGE,Prop)
+WideComp<-ExpandComp %>% group_by(YEAR,AGE) %>% select(YEAR,AGE,Prop) %>% spread(AGE,Prop)
+write.table(WideComp,file.path(outdir,"SurveyAgeComp.dat"), row.names = FALSE, quote = FALSE, sep = " ")
 return(WideComp)
 }
