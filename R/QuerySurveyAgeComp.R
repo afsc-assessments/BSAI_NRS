b3ae41f82475b07f7bd1a7cce97ded5f54b35e88
careymcg <carey.mcgilliard@gmail.com>
2022-09-29 13:51:42 -0700

    Updated QuerySurveyAgeComp.R
    did a lot of formatting

--- R/QuerySurveyAgeComp.R
+++ R/QuerySurveyAgeComp.R
@@ -1,7 +1,10 @@
 #Query the survey age comp data for unidentified and NRS in the EBS trawl survey
-
+library(RODBC)
+library(dplyr)
+library(tidyr)
 outdir = "C:\\Users\\carey.mcgilliard\\Work\\FlatfishAssessments\\2022\\NRS\\Data\\Survey_Comps"
-
+#Example:
+myages<-SurveyAgeComp(user = "mcgilliardc",pwd = "oopscircle12$",outdir = outdir)
 SurveyAgeComp<-function(user,pwd,outdir) {
 
 AFSC <- odbcConnect("AFSC","mcgilliardc","oopscircle12$") #mcgilliardc
@@ -22,12 +25,29 @@ thequery<-paste0("SELECT haehnr.agecomp_ebs_standard_stratum.species_code,\n ",
 
 ages<-sqlQuery(AFSC,thequery,believeNRows = FALSE)
 
-ages<-ages%>% filter(AGE!=-9)
-agecomp<-ages %>% group_by(YEAR,SEX,AGE) %>% summarise(CAges = sum( AGEPOP))
-agetot<-ages %>% group_by(YEAR,SEX) %>% summarise(TAges = sum( AGEPOP))
-Pagecomp<-full_join(agecomp,agetot) %>% mutate(Prop = CAges/TAges)
+ages<-ages%>% filter(AGE!=-9,AGE!=-99)
+#Bin plus group:
+ages <- ages %>% mutate(AGE = replace(AGE, AGE > 20, 20))
 
+#sum within year, sex, and age over unidentified and NRS and over plus group observations
+agecomp<-ages %>% group_by(YEAR,SEX,AGE) %>% summarise(CAges = sum( AGEPOP))
+
+#sum to get a total over ages to use for figuring out proportions at age
+agetot<-ages %>% group_by(YEAR,SEX) %>% summarise(TAges = sum( AGEPOP))
+
+#Turn age numbers into proportions
+Pagecomp<-full_join(agecomp,agetot) %>% mutate(Prop = CAges/TAges)
+Pagecomp<-Pagecomp %>% select(YEAR,SEX,AGE,Prop)
 #Look up whether it's females then males or vice versa for input to dat file
-write.csv(Pagecomp,file.path(outdir,"SurveyAgeComp.csv"))
-return(Pagecomp)
+
+
+#organize by females on left and males on right, bin at the plus group age
+thegrid<-expand.grid(AGE=seq(from =1,to=20,by=1),SEX = c(1,2),YEAR = unique(Pagecomp$YEAR))
+ExpandComp<-full_join(Pagecomp,thegrid) 
+ExpandComp<-ExpandComp %>% replace_na(list(Prop=0))
+ExpandComp$Prop<-format(round(ExpandComp$Prop,7),nsmall=7)
+
+WideComp<-ExpandComp %>% group_by(SEX,YEAR,AGE) %>% select(YEAR,AGE,Prop) %>% spread(AGE,Prop)
+write.csv(WideComp,file.path(outdir,"SurveyAgeComp.csv"))
+return(WideComp)
 }
