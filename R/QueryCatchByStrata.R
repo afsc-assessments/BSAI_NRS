724846cf3ef7b924cb2a55d7fdc283193d7e2a68
careymcg <carey.mcgilliard@gmail.com>
2022-10-24 13:29:55 -0700

    Updated QueryCatchByStrata.R
    query is written and just have to put it all together into a sam control file.

--- R/QueryCatchByStrata.R
+++ R/QueryCatchByStrata.R
@@ -2,11 +2,19 @@
 
 library(tidyverse)
 library(RODBC)
+library(lubridate)
                 
-AKFIN<- odbcConnect("AFSC","","") #mcgilliardc
+AKFIN<- odbcConnect("AKFIN","","") #cmcgilliard
 FmpArea <- 'BSAI' 
 SpeciesCode<-'RSOL' #and also 120
 
+#YFS StrataMap (strata are times of year here, but can also be NMFS_AREA or other)
+#StrataMap<-data.frame(STRATA =c(1,1,1,1,2,2,2,2,3,3,3,3),
+#                      MONTH = seq(from = 1,to = 12,by = 1)) #YFS: 3 strata over the months of the year
+
+#NRS StrataMap (only one strata for BSAI NRS right now)
+StrataMap<-data.frame(STRATA =rep(1,n = 12),
+                      MONTH = seq(from = 1,to = 12,by = 1)) #NRS: 1 strata
 
 MyQuery<-paste0("SELECT council.comprehensive_blend_ca.week_end_date,\n ",
 "council.comprehensive_blend_ca.catch_activity_date,\n ",
@@ -22,7 +30,7 @@ MyQuery<-paste0("SELECT council.comprehensive_blend_ca.week_end_date,\n ",
 "council.comprehensive_blend_ca.fmp_gear,\n ",
 "council.comprehensive_blend_ca.species_name\n ",
 "FROM council.comprehensive_blend_ca\n ",
-"WHERE council.comprehensive_blend_ca.species_group_code = 'RSOL'\n ",
-"AND council.comprehensive_blend_ca.fmp_area = 'BSAI'")
+"WHERE council.comprehensive_blend_ca.species_group_code =",SpeciesCode,"\n ",
+"AND council.comprehensive_blend_ca.fmp_area = ",FmpArea)
 
 catchbio<-sqlQuery(AKFIN,MyQuery)
